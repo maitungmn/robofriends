cefb482c6997b23f7ad452e6d6b62d04b61a0935
maitungmn <goingdownfr@gmail.com>
2019-07-24 15:15:46 +0700

    Updates

---
+++ precache-manifest.c87433861c07a71c04e2f25be5c8b9ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b5a8212d3830f3218c410706b0add7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e366e84635fd045abac0",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2549c98c02f412c6c404",
+    "url": "/robofriends/static/css/main.23d4545d.chunk.css"
+  },
+  {
+    "revision": "e366e84635fd045abac0",
+    "url": "/robofriends/static/js/2.142e9807.chunk.js"
+  },
+  {
+    "revision": "2549c98c02f412c6c404",
+    "url": "/robofriends/static/js/main.fe81259b.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "43e007bcca6e8a7c30ce5a999f0891fa",
+    "url": "/robofriends/static/media/Facon.43e007bc.woff"
+  }
+]);
